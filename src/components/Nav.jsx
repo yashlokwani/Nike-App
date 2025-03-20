947b7030ab032152903ab0e37652566c0c25b20d
yashlokwani <yashlokwani22@gmail.com>
2025-03-20 21:13:16 +0530

    Done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import {headerLogo} from '../assets/images'
 import {hamburger} from '../assets/icons'
-
+import {navLinks} from "../constants"
 import React from 'react'
 
 const Nav = () => {
@@ -11,7 +11,15 @@ const Nav = () => {
             <img src={headerLogo} alt="Logo" width={130} height={29} />
             </a>
 
-            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'></ul>
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+               {navLinks.map((item) => (<li key={item.label}>
+                                        <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                                          {item.label}</a></li>))}
+            </ul>
+
+            <div className='hidden max-lg:block'>
+              <img src={hamburger} alt="Hamburger" width={25} height={25}/>
+            </div>
         </nav>
     </header>
   )
